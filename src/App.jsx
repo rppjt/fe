7ae44ebba71354ed8ce48c34e030de735a5eb183
tro368@naver.com <tro368@naver.com>
2025-05-15 19:47:44 +0900

    feat: 카카오 로그인 버튼 컴포넌트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
-import LoginKakaoCallback from "./pages/LoginKakaoCallback";
+import LoginKakaoCallback from "./pages/LoginKakkoCallback";
 
 function App() {
   return (
