2177822fa73044152525c9dd4a1f6989a19022ae
tro368@naver.com <tro368@naver.com>
2025-05-22 20:34:42 +0900

    파일명

--- src/components/MaoContainer.jsx
+++ src/components/MaoContainer.jsx
@@ -3,10 +3,9 @@ import { useNavigate } from "react-router-dom";
 import { useKakaoMap } from "../hooks/useKakaoMap";
 import { useRunningTracker } from "../hooks/useRunningTracker";
 import { formatElapsedTime } from "../utils/timeUtils";
-import StartButton from "./buttons/StartButton";
-import StopButton from "./buttons/StopButton";
+import StartButton from "./StartButton";
+import StopButton from "./StopButton";
 import RunSummary from "./summaries/RunSummary";
-
 import styles from "./MapContainer.module.css";
 
 const MapContainer = () => {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,8 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { authFetch } from "../utils/authFetch";
-import LogoutButton from "./components/LogoutButton";
-import MapContainer from "./components/MapContainer";
+import LogoutButton from "../components/LogoutButton";
+import MapContainer from "../components/MapContainer";    
 
 const Home = () => {
   const [user, setUser] = useState(null);
