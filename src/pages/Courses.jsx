e4dc1a5687adac1f5bada1eb10e6853e5f72ce03
wontae <dnjsxo0616@gmail.com>
2025-05-26 19:49:22 +0900

    경로 수정

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import styles from "./courses.module.css";
+import styles from "./course.module.css";
 
 const Courses = () => {
   const [courses, setCourses] = useState([]);
